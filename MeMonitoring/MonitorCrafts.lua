5f18664d859f706f23ab6e2287a55fc20840fa48
Philsie <46480764+Philsie@users.noreply.github.com>
2025-11-10 05:08:41 +0100

    Improve line clearing logic in monitorCrafts
    Refactor monitorCrafts function to manage line clearing dynamically based on the number of items being crafted.

--- MeMonitoring/MonitorCrafts.lua
+++ MeMonitoring/MonitorCrafts.lua
@@ -4,27 +4,35 @@ local event = require("event")
 
 -- Component proxies
 local MeController = component.me_controller
+local Gpu = component.gpu
 
 -- Main Loop
 local function monitorCrafts()
-	term.clear()
-    term.setCursor(1, 1)
-    print("Items beeing Crafted:")
-    print("###############################")
+	local LinesToClear = 30
+    term.setCursor(1, 3)
 
     for i, Cpu in pairs(MeController.getCpus()) do
         if Cpu.busy == true then
             local finalOutputTable = Cpu.cpu.finalOutput()
             if finalOutputTable ~= nil then
+				LinesToClear=LinesToClear-3
                 print("Item:   ",finalOutputTable.label)
                 print("Amount: ",finalOutputTable.size)
                 print("-------------------------------")
             end
         end
     end
+
+	for i = 0, LinesToClear do print(" ") end
 end
 
 -- Main execution
+term.clear()
+
+print("Items beeing Crafted:")
+print("###############################")
+Gpu.setResolution(50,50)
+
 while true do
 	monitorCrafts()
 	event.pull(1, "timer")
