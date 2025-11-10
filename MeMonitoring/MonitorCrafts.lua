8a77f4100577cc0465888665e7141a67f745528f
Philsie <46480764+Philsie@users.noreply.github.com>
2025-11-10 04:57:39 +0100

    Add crafting monitor functionality in MonitorCrafts.lua

---
+++ MeMonitoring/MonitorCrafts.lua
@@ -0,0 +1,31 @@
+local component = require("component")
+local term = require("term")
+local event = require("event")
+
+-- Component proxies
+local MeController = component.me_controller
+
+-- Main Loop
+local function monitorCrafts()
+	term.clear()
+    term.setCursor(1, 1)
+    print("Items beeing Crafted:")
+    print("###############################")
+
+    for i, Cpu in pairs(MeController.getCpus()) do
+        if Cpu.busy == true then
+            local finalOutputTable = Cpu.cpu.finalOutput()
+            if finalOutputTable ~= nil then
+                print("Item:   ",finalOutputTable.label)
+                print("Amount: ",finalOutputTable.size)
+                print("-------------------------------")
+            end
+        end
+    end
+end
+
+-- Main execution
+while true do
+	monitorCrafts()
+end
+
